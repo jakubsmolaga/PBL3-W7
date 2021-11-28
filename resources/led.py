13ad39726f76994fb784dcd042e0c5569a7af8c8
Kuba Smolaga <smolagakuba@gmail.com>
2021-11-28 03:21:50 +0100

    more messing around

--- resources/led.py
+++ resources/led.py
@@ -24,15 +24,15 @@ class LED(Resource):
         self.args_parser.add_argument(name='b', type=inputs.int_range(0, 255))
 
     def get(self):
-        return {color: int(pin.value*255) for color, pin in pins.items()}
+        return {color: int(pin['value']*255) for color, pin in pins.items()}
 
     def post(self):
         args = self.args_parser.parse_args()
-        if args.r != None:
-            pins['r'].value = args.r / 255.0
-        if args.g != None:
-            pins['g'].value = args.g / 255.0
-        if args.b != None:
-            pins['b'].value = args.b / 255.0
+        if args['r'] != None:
+            pins['r'].value = args['r'] / 255.0
+        if args['g'] != None:
+            pins['g'].value = args['g'] / 255.0
+        if args['b'] != None:
+            pins['b'].value = args['b'] / 255.0
         logging.info(f'Color changed to {self.get()}')
         return self.get()
